ff3c82521c8a3ece0ce10511047d0dd3c84fab03
Daria Rezvin <109664398+dariarez@users.noreply.github.com>
2023-03-16 00:32:19 +0200

    Update p.py

--- p.py
+++ p.py
@@ -2,30 +2,26 @@ import requests
 import tkinter as tk
 
 
-m = input('enter the name of music: ')
+name_music = input('enter the name of music: ')
 
 window = tk.Tk()
 window.title('google')
 
 def music():
     url = "https://shazam.p.rapidapi.com/search"
-
-    querystring = {"term":"{}","locale":"en-US","offset":"0","limit":"5".format(m)}
-
+    querystring = {"term":"{}","locale":"en-US","offset":"0","limit":"5".format(name_music)}
     headers = {
 	    "X-RapidAPI-Key": "a8fda380femsh9ce422460c182f3p1d80a5jsn77c64e2ebc2a",
 	    "X-RapidAPI-Host": "shazam.p.rapidapi.com"
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-
     text = tk.Text()
     text.pack()
     text.insert('1.0', response.text)
 music()
 
-button = tk.Button(text='click me',  fg='white', bg='red', 
-                    command=music)
+button = tk.Button(text='click me',  fg='white', bg='red', command=music)
 button.pack()
 
 window.mainloop()
